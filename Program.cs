1016ed6b80aef34e12942047e0e76cd1a4295a07
Ilnurios325 <ipilulka@gmail.com>
2023-01-07 10:21:00 +0300

    adding

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter 1 num:");
+int num1 =
+Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter 2 num:");
+int num2 =
+Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (num1>num2)
+{
+    Console.WriteLine(max = (num1));
+}
+if (num1 == num2)
+{
+    Console.WriteLine("numbers are equal");
+}
+if (num1<num2)
+{
+    Console.WriteLine(max = (num2));
+}    
